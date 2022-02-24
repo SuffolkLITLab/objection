1bd0d5059df3e783061a40dad8996ebba90d2584
MattPat30 <mpatalano@su.suffolk.edu>
2022-02-24 17:27:28 -0500

    Testing if i can add text

--- js/scripts.js
+++ js/scripts.js
@@ -52,7 +52,7 @@ function object_type(type){
 		console.log("Overruled! "+type+" objection not found.")
 		$("#msg").css("background-color", "red");
 		$('#msg').show();
-		$('#msg').html("Overruled!  "+correct_type+"would have been granted")
+		$('#msg').html("Overruled! Testing if i can add to this")
 	}
 };
 
