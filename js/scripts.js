4e2da22f368376a3b4827647149f2d340cc9175f
MattPat30 <mpatalano@su.suffolk.edu>
2022-02-24 20:46:58 -0500

    Revert "Take 3? maybe"
    This reverts commit c8def924cb5c235e50a5ab2f4b7e3a6c9f053400.

--- js/scripts.js
+++ js/scripts.js
@@ -52,7 +52,7 @@ function object_type(type){
 		console.log("Overruled! "+type+" objection not found.")
 		$("#msg").css("background-color", "red");
 		$('#msg').show();
-		$('#msg').html("Overruled! Test")
+		$('#msg').html("Overruled! Testing if i can add to this")
 	}
 };
 
