589eca9d0f55492636abada27a0e141c3e074ef3
MattPat30 <mpatalano@su.suffolk.edu>
2022-02-25 10:34:18 -0500

    adding objections for video 1

--- js/videos.js
+++ js/videos.js
@@ -2,8 +2,12 @@ videos =  {
             videos: [
                 {url:"scRAK5sWEcM",
                 objections:[
-                              {seconds:1,type:"leading"},
-                              {seconds:10,type:"speculative"},
+                              {minutes:2,seconds:54,type:"leading"},
+                              {minutes:3,seconds:9,type:"leading"},
+                              {minutes:3,seconds:20,type:"leading"},
+                              {minutes:3,seconds:36,type:"leading"},
+                              {minutes:3,seconds:57,type:"leading"},
+                              {minutes:4,seconds:15,type:"leading"},
                             ]},
                 // videos below this not set up             
                 {url:"tUcer5uc7PE",
